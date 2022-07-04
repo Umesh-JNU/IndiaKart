dcdbf5b017d8d334449109c3c00cd043a45d4930
Umesh Kumar <umesh98_soe@jnu.ac.in>
2022-07-04 14:57:36 +0530

    user register/login

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,10 @@ app.get("/", (req, res) => {
 
 // route imports
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // middleware for errors
 app.use(errorMiddleware);
---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+const userModel = require('../models/userModal');
+const asyncMiddleware = require('../middleware/asyncMiddleware');
+const errorHandler = require('../utils/errorHandler');
+const sendToken = require('../utils/jwtTokens');
+
+// register user
+exports.registerUser = asyncMiddleware(async (req, res, next) => {
+    const {name, email, password} = req.body;
+
+    const user = await userModel.create({
+        name, email, password,
+        avatar: {
+            public_id: "this is a sample id",
+            url: "profileURL"
+        },
+    })
+
+    sendToken(user, 201, res);
+})
+
+// login user
+exports.loginUser = asyncMiddleware(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) 
+        return next(new errorHandler("Please enter your email and password", 400));
+
+    const user = await userModel.findOne({email}).select("+password");
+    console.log(user);
+
+    if(!user) {
+        return next(new errorHandler("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = user.comparePassword(password);
+
+    if(!isPasswordMatched) return next(new errorHandler("Invalid email or password!", 401));
+
+    sendToken(user, 200, res);
+});
+
