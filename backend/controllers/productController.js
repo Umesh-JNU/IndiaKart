432b9c8084e57717dae77a61c1b743bd339de285
Umesh Kumar <umesh98_soe@jnu.ac.in>
2022-07-07 11:05:29 +0530

    product update/review/etc...

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -77,3 +77,85 @@ exports.deleteProduct = asyncMiddleware(async (req, res, next) => {
         message: "Product Deleted successfully."
     })
 })
+
+exports.createProductReview = asyncMiddleware(async (req, res, next) => {
+    const {rating, comment, productId} = req.body;
+
+    const review = {
+        user: req.user._id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment
+    }
+
+    const product = await productModel.findById(productId);
+
+    const isReviewed = product.reviews.find(
+        (rev) => rev.user.toString() === req.user._id.toString()
+    );
+
+    if(isReviewed) {
+        product.reviews.forEach(rev => {
+            if(rev.user.toString() === req.user._id.toString()) (rev.rating = rating), (rev.comment = comment);
+        });
+    } else {
+        product.reviews.push(review);
+        product.numOfReviews = product.reviews.length
+    }
+
+    let avg = 0;
+    product.reviews.forEach(rev => {
+        avg += rev.rating;
+    });
+    product.ratings = avg / product.reviews.length;
+
+    await product.save({validateBeforeSave: false});
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+exports.getProductReviews = asyncMiddleware(async (req, res, next) => {
+    console.log(req.query)
+    const product = await productModel.findById(req.query.productId);
+
+    if(!product) {
+        return next(new ErrorHandler("Product Not found", 404));
+    }
+
+    res.status(200).json({
+        success: true, 
+        reviews: product.reviews
+    })
+})
+
+exports.deleteReview = asyncMiddleware(async (req, res, next) => {
+    const product = await productModel.findById(req.query.productId);
+
+    if(!product) {
+        return next(new ErrorHandler("Product Not found", 404));
+    }
+
+    const reviews = product.reviews.filter(
+        (rev) => rev._id.toString() != req.query.id.toString()
+    );
+
+    let avg = 0;
+    reviews.forEach(rev => avg += rev.rating);
+
+    const numOfReviews = reviews.length;
+    const ratings = avg / numOfReviews;
+
+    await productModel.findByIdAndUpdate(req.query.productId, {
+        reviews, ratings, numOfReviews
+    }, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true, 
+    })
+})
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
-const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword } = require('../controllers/userController')
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser, updateUserRole, deleteUser } = require('../controllers/userController')
 
 const express = require('express')
+const { isAuthenticated, authorizeRoles } = require('../middleware/auth')
 const router = express.Router()
 
 router.route("/register").post(registerUser)
@@ -8,5 +9,10 @@ router.route("/login").post(loginUser)
 router.route("/logout").get(logoutUser)
 router.route('/password/forgot').post(forgotPassword)
 router.route('/password/reset/:token').put(resetPassword)
+router.route('/me').get(isAuthenticated, getUserDetails)
+router.route('/password/update').put(isAuthenticated, updatePassword)
+router.route('/me/update').put(isAuthenticated, updateProfile)
+router.route('/admin/users').get(isAuthenticated, authorizeRoles("admin"), getAllUsers);
+router.route("/admin/user/:id").get(isAuthenticated, authorizeRoles("admin"), getSingleUser).put(isAuthenticated, authorizeRoles("admin"), updateUserRole).delete(isAuthenticated, authorizeRoles("admin"), deleteUser)
 
 module.exports = router
